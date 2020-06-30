ba413f95b5b60afc16ca553b2948eb93d275c878
Brasso12345 <cresaven@gmail.com>
2020-06-30 17:15:33 +0800

    removed dede

--- Main.py
+++ Main.py
@@ -6754,5 +6754,5 @@ def calculator(operation):
 
 print("MODE: a = Normal Calculator\n      "
                  "b = Sequence Calculator\n      "
-      "c = Conversion Calculator\n      d = dede")
+      "c = Conversion Calculator\n      d = ")
 calculator(input("Enter MODE: ").lower())
